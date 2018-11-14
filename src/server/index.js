0734573275f32892437abdf5816881f7318d1a32
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-11-14 08:50:27 +0000

    remove api call

--- src/server/index.js
+++ src/server/index.js
@@ -6,5 +6,4 @@ const PORT = 8000;
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
